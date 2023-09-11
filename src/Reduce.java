7de0a2ba50a0d5d0e1ddc8d0749af68f866ae0df
Samil Dhawan <samildhawan@MacBook-Pro.local>
2023-09-11 14:47:07 -0400

    Added Reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Reduce {
-}
+public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int i = 100;
+
+        while (i > 0) {
+            if (i % 2 == 0){
+                i /= 2;
+            } else {
+                i -= 1;
+            }
+            count ++;
+            }
+        System.out.println(count);
+        }
+    }
+

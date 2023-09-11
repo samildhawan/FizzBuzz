e925439c7e996047de656c5c2f22b583cb11f052
Samil Dhawan <samildhawan@MacBook-Pro.local>
2023-09-11 14:46:45 -0400

    Added Multiples.java

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+
+    public static void main(String[] args) {
+        int count = 0;
+        int i = 0;
+        while (i < 1000){
+            if (Multiple(i)) {
+                count++;
+            };
+            i++;
+        }
+        System.out.println(count);
+    }
+
+    public static boolean Multiple(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+        return divisibleBy3 || divisibleBy5;
+    }
 }
